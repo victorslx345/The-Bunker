f55ef5d8512b0edae2b2f20a79720a575d3cc88d
victorslx345 <victor509044@gmail.com>
2026-09-15 09:30:01 -0300

    Adicionado menu, e meia parte da cena inicial

---
+++ The-Bunker/README.md
@@ -0,0 +1,7 @@
+# The-Bunker
+Trabalho pratico
+Voce é Kris, um adolecente que mora em uma pequena cidade, voce ja viu tudo que havia na sua cidade, menos uma coisa, O Abrigo
+z - interagir
+x - voltar
+setinhas - move
+Colete itens para sair do abrigo, sobreviva.
---
+++ scripts/menu.gd
@@ -0,0 +1,22 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_jogar_pressed() -> void:
+	get_tree().change_scene_to_file("res://cenas/bunker.tscn") # Replace with function body.
+
+
+func _on_sair_pressed() -> void:
+	get_tree().quit() # Replace with function body.
+
+
+func _on_creditos_pressed() -> void:
+	pass # Replace with function body.
