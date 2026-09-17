5404396cff0fa59fe3c2d660aecf0b686e891e38
victorslx345 <victor509044@gmail.com>
2026-09-17 13:30:40 -0300

    Mais regresso
    como faz o personagem funcionar meu Deussssssssssssssssssssssss

--- scripts/kris.gd
+++ scripts/kris.gd
@@ -8,34 +8,22 @@ const SPEED = 300.0
 func _physics_process(delta: float) -> void:
 	velocity.x = 0
 	velocity.y = 0
-	#if Input.is_action_pressed('ui_up'):
-		#velocity.y = -1*SPEED
-		#$kris_sprites.play("andadno_cima")
-		#$kris_sprites.flip_h = false
-	#if Input.is_action_pressed('ui_down'):
-		#velocity.y = SPEED
-		#$kris_sprites.play("default")
-		#$kris_sprites.flip_h = false
-	#if Input.is_action_pressed('ui_right'):
-		#velocity.x = SPEED
-		#$kris_sprites.play("andando_lado")
-		#$kris_sprites.flip_h = true
-	#if Input.is_action_pressed('ui_left'):
-		#velocity.x = -1*SPEED
-		#$kris_sprites.play("andando_lado")
-		#$kris_sprites.flip_h = false
-		#move_and_slide()
-	if velocity.x > 0:
-		$kris_sprites.play("andando_lado")
-		$kris_sprites.flip_h = true
-	if velocity.x < 0:
-		$kris_sprites.play("andando_lado")
-		$kris_sprites.flip_h = false
-	if velocity.y < 0:
+
+	if Input.is_action_pressed('ui_up'):
+		velocity.y = -1*SPEED
 		$kris_sprites.play("andadno_cima")
 		$kris_sprites.flip_h = false
-	if velocity.y > 0:
+		$parado_up.is_visible_in_tree()
+	if Input.is_action_pressed('ui_down'):
+		velocity.y = SPEED
 		$kris_sprites.play("default")
-	if position.y != 120:
-		move_local_y(1)
-		$kris_sprites.play("default")
+		$kris_sprites.flip_h = false
+	if Input.is_action_pressed('ui_right'):
+		velocity.x = SPEED
+		$kris_sprites.play("andando_lado")
+		$kris_sprites.flip_h = true
+	if Input.is_action_pressed('ui_left'):
+		velocity.x = -1*SPEED
+		$kris_sprites.play("andando_lado")
+		$kris_sprites.flip_h = false
+	move_and_slide()
