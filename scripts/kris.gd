04483b5442fd24abc281d1c52e73390b22521a05
victorslx345 <victor509044@gmail.com>
2026-09-22 08:52:55 -0300

    Adcionando parte de dentro do bunker

--- scripts/kris.gd
+++ scripts/kris.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
 var mover = true
-const SPEED = 300.0
-var anim = true
+const SPEED = 200.0
+var anim = false
 func _ready() -> void:
-	pass
+	$idle.play("direita")
 
 func _physics_process(delta: float) -> void:
 	velocity.x = 0
@@ -15,10 +15,10 @@ func _physics_process(delta: float) -> void:
 			$kris_sprites.visible = false
 		if Input.is_action_pressed('ui_up'):
 			velocity.y = -1*SPEED
-			$kris_sprites.play("andadno_cima")
 			$kris_sprites.flip_h = false
 			$idle.visible = false
 			$kris_sprites.visible = true
+			$kris_sprites.play("andadno_cima")
 		if Input.is_action_pressed('ui_down'):
 			velocity.y = SPEED
 			$kris_sprites.play("default")
@@ -38,8 +38,8 @@ func _physics_process(delta: float) -> void:
 			$idle.visible = false
 			$kris_sprites.visible = true
 	move_and_slide()
-	
-#ANIMAÇAO~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#ANIMAÇAO~INICIAL~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 	if position.y != 120 and anim == true:
 		mover = false
@@ -48,8 +48,9 @@ func _physics_process(delta: float) -> void:
 		$idle.visible = false
 		$kris_sprites.play("default")
 	if position.y == 120:
-		$kris_sprites.visible = false
-		$idle.visible = true
 		mover = true
 		anim = false
 		$"../cenario/teto".disabled = false
+		
+func _on_anim_body_entered(body: Node2D) -> void:
+	anim = true
